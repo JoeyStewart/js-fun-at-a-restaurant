34da8fb6fb87bef8fc3733379cd7d219b1e79186
Joseph Stewart <JosephStewart1752@gmail.com>
2023-08-25 23:33:12 -0400

    Change

--- src/order.js
+++ src/order.js
@@ -1,7 +1,20 @@
 
+
+function takeOrder(order1, deliveryOrders) {
+  if(deliveryOrders.length < 3){
+  return deliveryOrders.push(order1)
+  } else {
+    return
+  }
+}
+
+function refundOrder(1657, deliveryOrders){
+  return deliveryOrders.remove(2893)
+}
+
 module.exports = {
-  // takeOrder,
-  // refundOrder,
+  takeOrder,
+  refundOrder,
   // listItems,
   // searchOrder
 }
