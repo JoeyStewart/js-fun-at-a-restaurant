a8c925c75b22c8916b3fecb0460efdf683490763
Joseph Stewart <JosephStewart1752@gmail.com>
2023-08-27 16:11:38 -0400

    Change

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,8 +1,36 @@
+// make an object
 
+// utilize object function to alter menu
+// if statment
+
+function createRestaurant(name) {
+  return {
+    name: name,
+    menus: {
+      breakfast: [],
+      lunch: [],
+      dinner: []
+    }
+  }
+}
+
+function addMenuItem(restaurant, items) {
+  var newArray = restaurant.menus[items.type]
+  for(var i = 0; i < newArray.length; i++){ 
+    if(newArray[i].name === items.name) { 
+      return
+    }
+  }
+  newArray.push(items)
+}
+
+function removeMenuItem(){
+  
+}
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
-  // removeMenuItem,
+  createRestaurant, 
+  addMenuItem,
+  removeMenuItem,
   // checkForFood
 }
