eb6824fc535e5df055c7dff4a523b7bd72fe5900
Joseph Stewart <JosephStewart1752@gmail.com>
2023-08-27 00:01:31 -0400

    Finish

--- src/order.js
+++ src/order.js
@@ -1,20 +1,58 @@
 
 
-function takeOrder(order1, deliveryOrders) {
+function takeOrder(order, deliveryOrders) {
   if(deliveryOrders.length < 3){
-  return deliveryOrders.push(order1)
+  return deliveryOrders.push(order)
   } else {
     return
   }
 }
 
-function refundOrder(1657, deliveryOrders){
-  return deliveryOrders.remove(2893)
+function refundOrder(orderNumber, deliveryOrders){
+  
+  for( i = 0; i < deliveryOrders.length; i++){
+    if(deliveryOrders[i].orderNumber === orderNumber) {
+      return deliveryOrders.splice([i], 1)
+  } 
+ }
 }
+
+function listItems(deliveryOrders) {
+  var items = []
+  for( i = 0; i < deliveryOrders.length; i++){
+    items.push(deliveryOrders[i].item)
+  }
+  return items.join(", ")
+}
+
+function searchOrder(deliveryOrders, itemOrder) {
+  
+  for( var i = 0; i < deliveryOrders.length; i++){
+    
+      if(deliveryOrders[i].item === itemOrder)
+      return true
+    } 
+      return false
+  }
+  
+
+
+// function searchOrder(deliveryOrders,itemName) {
+//   //item and includes is undefined
+// for (var i=0; i < deliveryOrders.length; i++) {
+//   if (deliveryOrders[i].item === itemName) 
+//   return true 
+//   }
+//   return false
+// }
+  
+  
+  
+
 
 module.exports = {
   takeOrder,
   refundOrder,
-  // listItems,
-  // searchOrder
+  listItems,
+  searchOrder
 }
